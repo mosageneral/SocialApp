8a822b1861a2d587316c275d202a803e8244ea06
Mosa <mhasan@onetelecom.me>
2021-09-22 11:58:20 +0200

    sdfsdf

--- Helper/MailSender.cs
+++ Helper/MailSender.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using AutoMapper.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Helper
 {
-    class MailSender
+    public static class MailSender
     {
+  
     }
 }
